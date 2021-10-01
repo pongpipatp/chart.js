e965175da69e2ac859f31e85b79aabda59008ac8
DESKTOP-LM4LJUA\Chopper <pongpipatp@kkumail.com>
2021-10-01 07:51:19 +0700

    [Added]: Charts

---
+++ components/ChartStack.vue
@@ -0,0 +1,53 @@
+<template>
+  <b-card title="Stack">
+    <b-card img-bottom>
+      <ChartBarBase :chart-data="chartData" />
+    </b-card>
+  </b-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartData: null
+    };
+  },
+  mounted() {
+    this.fillData();
+  },
+  methods: {
+    fillData() {
+      this.chartData = {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+
+        datasets: [
+          {
+            label: "Data One",
+            backgroundColor: "#f87979",
+            data: [40, 39, 10, 40, 39, 80, 40]
+          },
+          {
+            label: "Data Two",
+            backgroundColor: "#3D5B96",
+            data: [40, 39, 10, 40, 39, 80, 40]
+          },
+          {
+            label: "Data Three",
+            backgroundColor: "#1EFFFF",
+            data: [20, 10, 12, 33, 22, 4, 0]
+          }
+        ]
+      };
+    }
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>Chart Demo</h1>
+
+    <div class="grid">
+      <div class="chart">
+        <ChartBar />
+      </div>
+      <div class="chart">
+        <ChartDoughnut />
+      </div>
+      <div class="chart">
+        <ChartLine />
+      </div>
+      <div class="chart">
+        <ChartStack />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="css" scoped>
+.grid {
+  display: flex;
+  gap: 5%;
+  width: 50%;
+}
+
+.chart {
+  height: 60%;
+  width: 60%;
+}
+</style>
